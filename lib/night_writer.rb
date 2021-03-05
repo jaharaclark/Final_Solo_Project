e32bc9ae47845ec4b2617a7f893c9424f675fcc5
jaharaclark <jaharamclark@gmail.com>
2021-03-04 18:52:45 -1000

    Add remove comments and add char by index

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,11 +1,15 @@
-message = File.open("#{ARGV[0]}")
-braille = File.open("#{ARGV[1]}", 'w')
-
-while line = message.gets do
-    braille.write line
-end
+class NightWriter
   
+  message = File.open("#{ARGV[0]}")
+  braille = File.open("#{ARGV[1]}", 'w')
+  
+  # while line = message.gets do
+  #     braille.write line
+  # end
   characters = File.read("#{ARGV[0]}")
   count = characters.delete("\n")
+  
+  puts characters[0...4]
   puts "Created #{ARGV[1]} containing #{count.length} characters"
-message.close
+  message.close
+end
