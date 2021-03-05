efcd109c412d11d931844ca398d9890875a0cb39
jaharaclark <jaharamclark@gmail.com>
2021-03-04 18:04:45 -1000

    Add functionality to night writer to create output

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,23 @@
-puts 'Created braille.txt containing 256 characters.'
+# File.open(ARGV[0]).each do |line|
+#     puts line
+# end
+
+message = File.open("#{ARGV[0]}")
+braille = File.open("#{ARGV[1]}")
+
+while line = message.gets do
+    braille.write line
+end
+  
+  puts "Created #{braille}.txt containing 256 characters"
+message.close
+
+# ruby foo.rb test_list.txt
+
+# File.open("#{ARGV[1]}", 'w') do |braille|
+
+#     braille.puts "Ruby"
+#     braille.write "Java\n"
+#     braille << "Python\n"
+
+# end
