f79403f6e459ed6353d339a3b68923d2c9e44d64
jaharaclark <jaharamclark@gmail.com>
2021-03-08 11:04:45 -1000

    add capital, miniscule and write message tests

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -2,11 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'mocha/minitest'
 require './lib/night_writer'
+require './test/test_helper'
 
 class NightWriterTest < Minitest::Test
   def setup
     @night_writer = NightWriter.new
     @night_writer.stubs(:message).returns("hello world")
+    @night_writer.stubs(:braille).returns("hello world")
+    
   end
 
   def test_it_exists
@@ -14,14 +17,25 @@ class NightWriterTest < Minitest::Test
   end
 
   def test_translate
-    assert_equal "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n", @night_writer.translate
+    @night_writer.translate
+
+    assert_equal "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n", @night_writer.create_limited_output
   end
+
+  def test_it_can_make_capital
+    assert_equal ["..0.", "..00", ".0.."], @night_writer.make_capital("h")
+  end
+
+  def test_it_can_make_minuscule
+    assert_equal ["0.", "00", ".."], @night_writer.make_minuscule("h")
+  end
+
+  def test_it_can_write_message
+    braille = File.open("#{ARGV[1]}", "r")
+    assert_equal false, braille.read.empty?
+  end
+
+  
 
 end
 
-# read_message
-    # translate
-    # write_message
-    # confirmation
-    # make_capital(letter)
-    # make_minuscule(letter)
