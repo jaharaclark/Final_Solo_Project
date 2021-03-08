743e824fda01d7ca47f785d5ed346956994504cf
jaharaclark <jaharamclark@gmail.com>
2021-03-07 19:19:13 -1000

    created limited output method, remove line breaks

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -12,11 +12,11 @@ class NightWriter
   def perform
     read_message
     translate_message
-    insert_line_breaks
+    create_limited_output
     write_message
     confirmation
   end
-  
+
   def read_message
     @message = File.open("#{ARGV[0]}").read
   end
@@ -34,42 +34,16 @@ class NightWriter
     create_limited_output
   end
 
-  def write_message
-    braille = File.open("#{ARGV[1]}", "w")
-    # braille.write @transposed.join("")
-    @transposed.each do |element|
-      joined = element.join("")
-      braille.write joined
-      braille.write "\n"
-    end
-  end
-
-  def insert_line_breaks
-    # take transposed, chop off first three arrays after 40
-    # take that result to set up next three arrays, keep chopping until under 40
-
-    # [0..39] 40 char slices when unjoined
-    # [0..79] 80 char slices when joined
-
-
-    # braille_message = @transposed.flatten.join("")
-    # braille_for_print = braille_message.scan(/.{1,80}/)
-
-    # x = []
-    # @transposed.each {|element| x << element.join("").scan(/.{1,80}/)}
-    # @transposed = x.flatten
-    # require 'pry'; binding.pry
-
-    
-
-    @transposed.map do |row|
-        break_indicies.reverse.map do |index|
-        row.insert(index+1, "!!!!!!!!!!!!!!!")
+   def create_limited_output
+    while @braille_result[0] != "" do
+      @braille_result.each do |braille_letter|
+        @print_output << braille_letter.slice!(0..79) + "\n"
       end
     end
-
-    @transposed 
+    @print_output
   end
+
+  
 
   def confirmation
     p "Created '#{ARGV[1]}' containing #{@message.length} characters"
@@ -79,29 +53,3 @@ end
 runner = NightWriter.new
 runner.perform
 
-# "A" => ["..0.", "....", ".0.."],
-#       "B" => ["..0.", "..0.", ".0.."],
-#       "C" => ["..00", "....", ".0.."],
-#       "D" => ["..00", "...0", ".0.."],
-#       "E" => ["..0.", "...0", ".0.."],
-#       "F" => ["..00", "..0.", ".0.."],
-#       "G" => ["..00", "..00", ".0.."],
-#       "H" => ["..0.", "..00", ".0.."],
-#       "I" => ["...0", "..0.", ".0.."],
-#       "J" => ["...0", "..00", ".0.."],
-#       "K" => ["..0.", "....", ".00."],
-#       "L" => ["..0.", "..0.", ".00."],
-#       "M" => ["..00", "....", ".00."],
-#       "N" => ["..00", "...0", ".00."],
-#       "O" => ["..0.", "...0", ".00."],
-#       "P" => ["..00", "..0.", ".00."],
-#       "Q" => ["..00", "..00", ".00."],
-#       "R" => ["..0.", "..00", ".00."],
-#       "S" => ["...0", "..0.", ".00."],
-#       "T" => ["...0", "..00", ".00."],
-#       "U" => ["..0.", "....", ".000"],
-#       "V" => ["..0.", "..0.", ".000"],
-#       "W" => ["...0", "..00", ".0.0"],
-#       "X" => ["..00", "....", ".000"],
-#       "Y" => ["..00", "...0", ".000"],
-#       "Z" => ["..0.", "...0", ".000"],
