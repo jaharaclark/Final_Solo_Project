df14c1ca5e13dde96ebba8e0f98b62c7de10c353
jaharaclark <jaharamclark@gmail.com>
2021-03-07 21:30:49 -1000

    finish letter methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,31 +1,29 @@
 require './lib/dictionary'
 class NightWriter
-  include Dictionary 
-
-  attr_reader :cipher, :message, :output, :rendered, :transposed
-
+  include Dictionary
+  attr_reader :cipher, :message, :print_output, :rendered, :transposed, :braille_result
+  
   def initialize
     @cipher = dictionary
-    @output = []
+    @print_output = ""
+    @braille_result = ["", "", ""]
   end
-  
+
   def perform
     read_message
-    translate_message
-    create_limited_output
+    translate
     write_message
     confirmation
   end
 
   def read_message
-    @message = File.open("#{ARGV[0]}").read
+    @message = File.open("#{ARGV[0]}").read.delete("\n")
   end
-  
-  
-  def translate_message
+
+  def translate
     @message = @message.gsub("\n", " ")
     @message.each_char do |letter|
-       if letter =~ /[A-Z]/ 
+       if letter =~ /[A-Z]/
          make_capital(letter)
        else
          make_minuscule(letter)
@@ -34,7 +32,7 @@ class NightWriter
     create_limited_output
   end
 
-   def create_limited_output
+  def create_limited_output
     while @braille_result[0] != "" do
       @braille_result.each do |braille_letter|
         @print_output << braille_letter.slice!(0..79) + "\n"
@@ -43,13 +41,30 @@ class NightWriter
     @print_output
   end
 
-  
+  def make_capital(letter)
+    cap = @cipher["capitalize"]
+    braille_letter = @cipher[letter.downcase]
+    @braille_result[0] = @braille_result[0] + cap[0] + braille_letter[0]
+    @braille_result[1] = @braille_result[1] + cap[1] + braille_letter[1]
+    @braille_result[2] = @braille_result[2] + cap[2] + braille_letter[2]
+    @braille_result
+  end
 
+  def make_minuscule(letter)
+    braille_letter = @cipher[letter]
+    @braille_result[0] = @braille_result[0] + braille_letter[0]
+    @braille_result[1] = @braille_result[1] + braille_letter[1]
+    @braille_result[2] = @braille_result[2] + braille_letter[2]
+  end
+
+  def write_message
+    braille = File.open("#{ARGV[1]}", "w")
+    braille.write @print_output
+  end
+  
   def confirmation
-    p "Created '#{ARGV[1]}' containing #{@message.length} characters"
+    puts "Created '#{ARGV[1]}' containing #{@message.length} characters"
   end
 end
-
 runner = NightWriter.new
 runner.perform
-
